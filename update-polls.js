1409c3d60e3009ee5830f041e5fb7da1f9b920c1
Adam Hooper <adam@adamhooper.com>
2016-09-07 11:30:36 -0400

    Make update-polls.js output to stdout

--- update-polls.js
+++ update-polls.js
@@ -2,7 +2,6 @@
 
 'use strict'
 
-const fs = require('fs')
 const csv_parse = require('csv-parse/lib/sync')
 const sync_request = require('sync-request')
 
@@ -11,7 +10,7 @@ const GoogleSheets = require('./generator/GoogleSheets')
 
 function get_race_tsv_lines(race) {
   const url = `http://elections.huffingtonpost.com/pollster/api/charts/${race.pollster_slug}.csv`
-  console.log(`GET ${url}`)
+  console.warn(`GET ${url}`)
   const text = sync_request('GET', url).getBody()
   const rows = csv_parse(text, { columns: true })
   return rows.map(row => `${race.state}\t${row.end_date}\t${parseFloat(row[race.dem_label]) - parseFloat(row[race.gop_label])}`)
@@ -30,9 +29,7 @@ function refresh_polls_tsv() {
   const lines = get_all_race_tsv_lines(races)
   const text = `state\tend_date\tdem_lead\n${lines.join('\n')}`
 
-  const filename = `${__dirname}/data/sheets/output/senate-polls.tsv`
-  console.log(`WRITE ${filename}`)
-  fs.writeFileSync(filename, text)
+  console.log(text)
 }
 
 refresh_polls_tsv()
