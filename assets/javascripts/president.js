7ddbb5cf7e2f5b37dbb838646c02926d5a058da4
Adam Hooper <adam@adamhooper.com>
2016-09-26 13:46:25 -0400

    Tweak histograms

--- assets/javascripts/president.js
+++ assets/javascripts/president.js
@@ -149,7 +149,7 @@ function make_win_probabilities_histograms() {
     var width = aContainer.clientWidth;
     var height = aContainer.clientHeight - parseFloat(containerStyle.paddingTop) - parseFloat(containerStyle.paddingBottom);
 
-    var nBuckets = Math.floor((width + 2) / 5);
+    var nBuckets = Math.floor((width + 1) / 5);
     // We want an odd number of buckets, so the mean is highest
     if (nBuckets % 2 === 0) nBuckets -= 1;
     //var nBuckets = width;
@@ -169,7 +169,7 @@ function make_win_probabilities_histograms() {
         var max = histogram.min + bucketSize * (i + 1);
         var isMean = (min <= histogram.mean) === (max >= histogram.mean);
         var fraction = barFraction(min, max, histogram.mean, histogram.stddev);
-        var color = (min <= 0 && max >= -0) ? tossUp : (min > 0 ? (isMean ? strongDem : mutedDem) : (isMean ? strongGop : mutedGop));
+        var color = (min <= 0 === max >= -0) ? tossUp : (min > 0 ? (isMean ? strongDem : mutedDem) : (isMean ? strongGop : mutedGop));
 
         buckets.push({
           color: color,
@@ -181,8 +181,8 @@ function make_win_probabilities_histograms() {
     });
 
     // x0: left of first rect. Each bucket is 5px wide, with the rect in the
-    // middle 3px. The first and last buckets are only 4px wide, so the rect is
-    // flush to the edge in those cases. That's why there's no "+ 1" here.
+    // rightmost 4px. The first bucket is only 4px wide, so the rect is flush
+    // to the edge in that one cases. That's why there's no "+ 1" here.
     var x0 = Math.floor((width - (bucketWidth * nBuckets)) / 2);
     //var x0 = 0;
 
@@ -200,7 +200,7 @@ function make_win_probabilities_histograms() {
         var h = height * bucket.fraction / maxFraction;
 
         ctx.fillStyle = bucket.color;
-        ctx.fillRect(x, height - h, 3, h);
+        ctx.fillRect(x, height - h, 4, h);
       });
     });
   }
