2a656d261b9d4bb44f8fc17f6d624fd994351df8
Adam Hooper <adam@adamhooper.com>
2016-09-26 12:54:34 -0400

    Write presidential "ol.calculations"

--- app/PresidentRace.js
+++ app/PresidentRace.js
@@ -34,10 +34,15 @@ module.exports = class PresidentRace {
 
     this.diff_xibar = 100 * (+summary.diff_xibar)
     this.diff_stddev = 100 * (+summary.diff_stddev)
+    this.diff_margin = this.diff_stddev * 1.96
     this.undecided_stddev_boost = 100 * (+summary.undecided_stddev_boost)
+    this.undecided_margin_boost = this.undecided_stddev_boost * 1.96
 
     this.final_diff_xibar = this.diff_xibar
+    this.final_diff_leader = this.final_diff_xibar > 0 ? 'clinton' : 'trump'
+    this.final_diff_leader_name = this.final_diff_xibar > 0 ? 'Clinton' : 'Trump'
     this.final_diff_stddev = this.diff_stddev + this.undecided_stddev_boost
+    this.final_diff_margin = this.final_diff_stddev * 1.96
 
     this.curve = curve
     this.toss_up = this.dem_win_prob_with_adjustment_and_undecided === 0.5
