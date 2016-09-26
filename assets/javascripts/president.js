6c5297f0a57578922e8da35f2c502d3794a2bd73
alissascheller <alissascheller@gmail.com>
2016-09-26 12:42:43 -0400

    get rid of tooltip again

--- assets/javascripts/president.js
+++ assets/javascripts/president.js
@@ -96,7 +96,7 @@ function handle_hover_on_vote_counts() {
     focusBar(tallestBar);
   }
 
-  tooltip.style.display = 'block';
+  tooltip.classList.remove('loading');
   focusBar(tallestBar);
   bars_el.addEventListener('mousemove', focus);
   bars_el.addEventListener('mouseleave', leave);
